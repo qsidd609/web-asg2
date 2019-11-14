a7d036785f73136cf865ae23aff5611f19f0b08a
runveerb <55037066+runveerb@users.noreply.github.com>
2019-11-14 13:55:10 -0700

    Add files via upload

---
+++ Assignment2/Profile-Page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>COMP 3512 Assign2</title>
+        <link rel="stylesheet" href="css/Desktop_Stylesheet.css" media="screen and (min-width: 600px)">
+        <link rel="stylesheet" href="css/Mobile_Stylesheet.css" media="screen and (max-width:600px)">
+    </head>
+    <body id="profilePageBody">
+        <header id="profilePageHeader"><h3>Header</h3></header>
+        <div id="profilePageUserInfo">
+            <h3>User Info</h3>
+        </div>
+        <div id="profilePageSearch">
+            <input type="text" class="searchForPhoto" placeholder="Search Box For Photos" list="filterList">
+        </div>
+        <div id="profilePageFavoritedImages">
+            <h3>Favorited Images</h3>
+        </div>
+        <div id="profilePageUserImagesMayLike">
+            <h3>Images You May Like</h3>
+        </div>
+    </body>
+</html>
