f40a0eb7ecb6e79a9c51b1c1d9dcd0b115c7a028
Mandeep G <57504395+MG-Singh@users.noreply.github.com>
2019-12-06 13:04:08 -0700

    Delete config.inc.php

--- Browse&SinglePhoto/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'travel');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
-?>
